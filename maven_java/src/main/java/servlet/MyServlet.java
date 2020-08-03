8f3dd268c485083c0d1954d238f50b1afa05d061
think <864581096@qq.com>
2020-08-03 21:05:46 +0800

    将java工程添加到版本库

---
+++ maven_java/src/main/java/servlet/MyServlet.java
@@ -0,0 +1,14 @@
+package servlet;
+
+import java.io.IOException;
+
+public class MyServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        this.doGet(request,response);
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        response.setContentType("text/html;charset=utf-8");
+        response.getWriter().write("Hello Mven Web");
+    }
+}
